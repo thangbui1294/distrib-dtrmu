a6f45ca39c39bbcac58f196b107225b6faaf7407
Thang Bui <thangbui1294@gmail.com>
2020-11-12 00:54:15 -0500

    minor change to test

--- run-node.bash
+++ run-node.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 export PYTHONPATH="/home/ec2-user/distrib-dtrmu/distalgo-master":${PYTHONPATH} 
 chmod 777 /home/ec2-user/distrib-dtrmu/main.da
-python3 -m da -n $1 -D /home/ec2-user/distrib-dtrmu/main.da
