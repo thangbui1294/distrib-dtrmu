229aa590f32dad4c29a9dcbd5b07e5538cb56c1a
Thang Bui <thangbui1294@gmail.com>
2020-11-12 00:52:32 -0500

    update scripts to run setup at worker nodes

--- run-node.bash
+++ run-node.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
+export PYTHONPATH="/home/ec2-user/distrib-dtrmu/distalgo-master":${PYTHONPATH} 
 chmod 777 /home/ec2-user/distrib-dtrmu/main.da
 python3 -m da -n $1 -D /home/ec2-user/distrib-dtrmu/main.da
