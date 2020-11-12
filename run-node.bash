83cb5af14db15fe6bf629eadd8aac97f97a4ae16
Thang Bui <thangbui1294@gmail.com>
2020-11-12 10:46:16 -0500

    Update run-node.bash

--- run-node.bash
+++ run-node.bash
@@ -1,2 +1,5 @@
 #!/bin/bash
 export PYTHONPATH="/home/ec2-user/distrib-dtrmu/distalgo-master":${PYTHONPATH}
+rm -f ~/main.da.log
+chmod 777 /home/ec2-user/distrib-dtrmu/main.da
+python3 -m da -n $1 -D /home/ec2-user/distrib-dtrmu/main.da
