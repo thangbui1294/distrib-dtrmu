93be18c1658e921551836bd70b91086aa09fa5e3
Thang Bui <thangbui1294@gmail.com>
2020-11-17 11:15:50 -0500

    update config files

--- mining-algorithms/DTRMU/configs/generat_distrib_configs.py
+++ mining-algorithms/DTRMU/configs/generat_distrib_configs.py
@@ -9,7 +9,7 @@ for i in range(4):
                     if 'num_policies=' in line:
                         f_out.write('num_policies=1\nrun_policy=' + str(j) + '\n')
                     elif 'output_file=' in line:
-                        f_out.write('output_file=../output/scaling_f_3/output_eWorkforce_10_' + str(j) + '.txt\n')
+                        f_out.write('output_file=../output/scaling_f_' + str(i) + '/output_eWorkforce_10' + '_' + str(j) + '.txt\n')
                     else:
                         f_out.write(line + '\n')
 
--- mining-algorithms/improve-policy/configs/generate_distrib_configs.py
+++ mining-algorithms/improve-policy/configs/generate_distrib_configs.py
@@ -9,7 +9,7 @@ for i in range(4):
                     if 'numPoliciesPerSize=' in line:
                         f_out.write('numPoliciesPerSize=1\nrunPolicy=' + str(j) + '\n')
                     elif 'outputFile=' in line:
-                        f_out.write('outputFile=../../output/scaling_f_3/output_eWorkforce_10_' + str(j) + '.txt\n')
+                        f_out.write('outputFile=../../output/scaling_f_' + str(i) + '/output_eWorkforce_10_' + str(j) + '.txt\n')
                     else:
                         f_out.write(line + '\n')
 
