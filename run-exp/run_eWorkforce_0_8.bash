af9f61182d1dabb4c98a3b21b5750a17ad7a5ad3
Thang Bui <thangbui1294@gmail.com>
2020-11-19 15:59:20 -0500

    add run bash files for new policies

---
+++ run-exp/run_eWorkforce_0_8.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/ec2-user/distrib-dtrmu/mining-algorithms/learning-data-generator
+cd bin
+java -Xms3g -Xmx3g -cp ".:../libs/commons-lang3-3.4.jar" learningdatagenerator/LearningDataGenerator ../configs/eWorkforce_0_8.conf
+cd ../..
+cd DTRMU
+python3 main.py ./configs/eWorkforce_0_8.conf 
+cd ../improve-policy/
+cd bin
+java -Xms3g -Xmx3g -cp ".:../libs/commons-math-2.2.jar:../libs/commons-lang3-3.4.jar:../libs/commons-math3-3.3.jar:../libs/jdi.jar;" algo/ReBACMiner ../configs/eWorkforce_0_8.conf
